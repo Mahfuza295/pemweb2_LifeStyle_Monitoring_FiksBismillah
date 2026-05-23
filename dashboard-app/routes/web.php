9e9328821ff860be27e88e6f93af481bb87792f1
MahfuzaDinia <rahmahmahfuzadinia@gmail.com>
2026-05-23 08:10:01 +0800

    membuat kode web php

--- dashboard-app/routes/web.php
+++ dashboard-app/routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Arahkan halaman utama langsung ke dashboard
+Route::get('/', [PageController::class, 'dashboard']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Navigasi Menu Dashboard
+Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+Route::get('/aktivitas', [PageController::class, 'aktivitas'])->name('aktivitas');
+Route::get('/artikel', [PageController::class, 'artikel'])->name('artikel');
+Route::get('/profil', [PageController::class, 'profil'])->name('profil');
