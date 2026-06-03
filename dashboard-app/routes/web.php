651068b643c29c9c8a03909103d6f146dc1f7ea8
ObedAlexander <obed120926@gmail.com>
2026-06-03 15:38:37 +0800

    Update comments and add route for storing activities

--- dashboard-app/routes/web.php
+++ dashboard-app/routes/web.php
@@ -4,13 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ArtikelController;
 
-//halaman utama langsung ke dashboard
+// halaman utama langsung ke dashboard
 Route::get('/', [PageController::class, 'dashboard']);
 
-//navigasi Menu Dashboard
+// navigasi menu utama
 Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
 Route::get('/aktivitas', [PageController::class, 'aktivitas'])->name('aktivitas');
+Route::post('/aktivitas', [PageController::class, 'storeAktivitas'])->name('aktivitas.store');
 Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.index');
 Route::get('/profil', [PageController::class, 'profil'])->name('profil');
-
-
